3bb3f7446c666c01db381e03d69e1f48804e8651
Yasin Samet Karaca <ysk@outlook.com.tr>
2024-03-13 18:07:03 +0300

    refactor: update ui

--- components/chat/AdminControls.tsx
+++ components/chat/AdminControls.tsx
@@ -3,7 +3,7 @@ import DeleteChatButton from "@/components/chat/DeleteChatButton";
 
 function AdminControls({chatId}: {chatId: string}) {
     return (
-        <div className="flex justify-end space-x-2 m-5 mb-0">
+        <div className="flex justify-center md:justify-end space-x-1 m-5 mb-0">
             <InviteUser chatId={chatId} />
             <DeleteChatButton chatId={chatId} />
         </div>
